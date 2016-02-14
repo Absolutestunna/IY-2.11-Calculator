6d38baefa7f59edc13119f24e5eca0fd9f577478
Home <Home@Homes-MacBook-Pro.local>
2016-02-14 17:12:31 -0500

    safe point

--- main.js
+++ main.js
@@ -21,74 +21,106 @@
     var eq = document.querySelector(".equals");
     var disp = document.querySelector(".display p");
 
+  //Clear and clear all definitions
+    var c = document.querySelector(".clear");
+    var cA = document.querySelector(".clearAll");
+
   //global Variables
     var num = "";     //selected number
     var newnum = "";  //selected updated number;
     total = "";
-    var sel_op = "";  //selected operator;
+    var sel_op1 = "";  //selected operator;
+    var sel_op2 = "";
+    var counter = "";
+    dispLength = "";
 
 
   //pushNumber functions
     function pushNumber() {
         num += this.textContent;
         disp.textContent = num;
+        dispLength = (disp.textContent).length;
+
     }
   //pushOps functions
     function pushOperatorP() {
-      sel_op = this.textContent;
-      console.log(sel_op)
+      sel_op1 = this.textContent;
       newnum = num;
-      disp.textContent = newnum + sel_op;
+      disp.textContent = newnum + sel_op1;
       num = "";
+      counter++;
 
     }
     function pushOperatorM() {
-      sel_op = this.textContent;
-      console.log(sel_op)
+      sel_op1 = this.textContent;
+      console.log(sel_op1)
       newnum = num;
-      disp.textContent = newnum + sel_op;
+      disp.textContent = newnum + sel_op1;
       num = "";
+      counter++;
 
     }
     function pushOperatorMP() {
-      sel_op = this.textContent;
-      console.log(sel_op)
+      sel_op1 = this.textContent;
+      console.log(sel_op1)
       newnum = num;
-      disp.textContent = newnum + sel_op;
+      disp.textContent = newnum + sel_op1;
       num = "";
+      counter++;
 
     }
     function pushOperatorD() {
-      sel_op = this.textContent;
-      console.log(sel_op)
+      sel_op1 = this.textContent;
+      console.log(sel_op1)
       newnum = num;
-      disp.textContent = newnum + sel_op;
+      disp.textContent = newnum + sel_op1;
       num = "";
+      counter++;
 
     }
 
     //function for calculations
     function calculate() {
-      if (sel_op === "+"){
+      if (sel_op1 === "+"){
         total = parseInt(newnum,10) + parseInt(num,10);
         disp.textContent = total;
+        newnum = "";
+        num = "";
+        console.log(counter);
       }
-      if (sel_op === "-"){
+      if (sel_op1 === "-"){
         total = parseInt(newnum,10) - parseInt(num,10);
         disp.textContent = total;
+        newnum = "";
+        num = "";
       }
-      if (sel_op === "/"){
+      if (sel_op1 === "/"){
         total = parseInt(newnum,10) / parseInt(num,10);
         disp.textContent = total;
+        newnum = "";
+        num = "";
       }
-      if (sel_op === "*"){
+      if (sel_op1 === "*"){
         total = parseInt(newnum,10) * parseInt(num,10);
         disp.textContent = total;
         newnum = "";
         num = "";
       }
+    }//end of calculate functions
+
+    //functions fo clear and clearAll
+    function eraseOne() {
+
     }
-    //numbers selectors
+    function eraseAll() {
+      num = "";
+      newnum = "";
+      disp.textContent = "0";
+      counter = "";
+      }
+
+
+    //numbers events
     one.addEventListener("click", pushNumber);
     two.addEventListener("click", pushNumber);
     three.addEventListener("click", pushNumber);
@@ -100,13 +132,17 @@
     nine.addEventListener("click", pushNumber);
     zero.addEventListener("click", pushNumber);
 
-    //operators selectors
+    //operators events
      plus.addEventListener("click", pushOperatorP);
      minus.addEventListener("click", pushOperatorM);
      multiply.addEventListener("click", pushOperatorMP);
      divide.addEventListener("click", pushOperatorD);
 
+     //equals/calculate event
      eq.addEventListener("click", calculate);
 
+     //clear and clearAll events
+     c.addEventListener("click", eraseOne);
+     cA.addEventListener("click", eraseAll);
 
 }());
