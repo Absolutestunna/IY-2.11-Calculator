a0976eb5a43650d58e0acd04d79b9b613dbadd19
Joshua Abu <joshujah007@gmail.com>
2016-02-16 12:14:07 -0500

    update

--- main.js
+++ main.js
@@ -27,110 +27,167 @@
     var cA = document.querySelector(".clearAll");
   //
   // //global Variables
-    var num = "";     //selected number
-    var newnum = "";  //selected updated number;
-    var total = "";
-    var sel_op1 = "";  //selected operator;
-    var sel_op2 = "";
-    var num_counter = 0;
-    var counter = 0;
-    dispLength = "";
+    // var num = "";     //selected number
+    // var newnum = "";  //selected updated number;
+    // var total = "";
+    // var sel_op1 = "";  //selected operator;
+    // var sel_op2 = "";
+    // var num_counter = 0;
+    // var counter = 0;
+    // dispLength = "";
+    // var link = 0;
 
 
-  // pushNumber functions
-    function pushNumber() {
-        num += this.textContent;
-        disp.textContent = num;
-        // num_counter++;
+  // // pushNumber functions
+  //   function pushNumber() {
+  //       num += this.textContent;
+  //       disp.textContent = num;
+  //       // num_counter++;
+  //
+  //
+  //   }
+  // // pushOps functions
+  //   function pushOperatorP() {
+  //     sel_op1 = this.textContent;
+  //     newnum = num;
+  //     disp.textContent = newnum;
+  //     num = "";
+  //     counter++;
+  //     if (counter > 1) {
+  //
+  //     }
+  //
+  //   }
+  //
+  //   function pushOperatorM() {
+  //     sel_op1 = this.textContent;
+  //     newnum = num;
+  //     disp.textContent = newnum + sel_op1;
+  //     num = "";
+  //     counter++;
+  //
+  //     // if (counter > 1) {
+  //     //   sel_op1=this.textContent;             //prepare for new operation
+  //     //
+  //     // }
+  //   }
+  //
+  //   function pushOperatorMP() {
+  //     sel_op1 = this.textContent;
+  //     newnum = num;
+  //     disp.textContent = newnum + sel_op1;
+  //     num = "";
+  //     counter++;
+  //   }
+  //   function pushOperatorD() {
+  //
+  //     sel_op1 = this.textContent;
+  //     newnum = num;
+  //     disp.textContent = newnum + sel_op1;
+  //     num = "";
+  //     counter++;
+  //    }
+  //
+  //   //function for calculations
+  //   function calculate() {
+  //     if (sel_op1 === "+"){
+  //       total = parseInt(newnum,10) + parseInt(num,10);
+  //     }
+  //     else if (sel_op1 === "-"){
+  //       total = parseInt(newnum,10) - parseInt(num,10);
+  //
+  //     }
+  //     else if (sel_op1 === "/"){
+  //       total = parseInt(newnum,10) / parseInt(num,10);
+  //
+  //     }
+  //     else if (sel_op1 === "*"){
+  //       total = parseInt(newnum,10) * parseInt(num,10);
+  //
+  //     }
+  //    }//end of calculate functions
+  //
+  //    function equalize() {
+  //       calculate();
+  //       disp.textContent = total;
+  //       newnum = "";
+  //       num = "";
+  //
+  //    }
+  //   //functions fo clear and clearAll
+  //   function eraseOne() {
+  //
+  //   }
+  //   function eraseAll() {
+  //     num = "";
+  //     newnum = "";
+  //     disp.textContent = "0";
+  //     num_counter = "";
+  //     }
 
-    }
-  // pushOps functions
-    function pushOperatorP() {
-      sel_op1 = this.textContent;
-      newnum = num;
-      disp.textContent = newnum;
-      num = "";
-      num_counter++;
+  var num = "";     //selected number
+  var newnum = "";  //selected updated number;
+  var total = "";
+  var sel_op = "";  //selected operator;
+  var calc_array = [];
+  var selected = "";
 
-      if (num_counter > 1){
-        calculate();
-        console.log(total)
+  function pushNumber() {
+    num += this.textContent
+    calc_array.push(num);
+    disp.textContent = num;
+  }
 
-        disp.textContent = total;
+  function pushOperatorP() {
+    sel_op = this.textContent;
+    calc_array.push(sel_op);
+    disp.textContent = sel_op;
+    console.log(calc_array);
 
-        // newnum = "";
-        // num = "";
-      }
+    // selected = calc_array.slice((calc_array.indexOf(sel_op))-1), (calc_array.indexOf(sel_op))
+    // disp.textContent = selected.slice(0, 1);
+    // calc_array = selected;
+
+  }
+  function pushOperatorM() {
+    sel_op = this.textContent;
+    calc_array.push(sel_op);
+    disp.textContent = sel_op;
+    console.log(calc_array);
 
 
-    }
+    // selected = calc_array.slice((calc_array.indexOf(sel_op))-1), (calc_array.indexOf(sel_op))
+    // disp.textContent = selected.slice(0, 1);
+    // calc_array = selected;
 
-    function pushOperatorM() {
-      sel_op1 = this.textContent;
-      newnum = num;
-      disp.textContent = newnum + sel_op1;
-      num = "";
-      counter++;
 
-      // if (counter > 1) {
-      //   sel_op1=this.textContent;             //prepare for new operation
-      //
-      // }
-    }
 
-    function pushOperatorMP() {
-      sel_op1 = this.textContent;
-      newnum = num;
-      disp.textContent = newnum + sel_op1;
-      num = "";
-      counter++;
-    }
-    function pushOperatorD() {
+  }
+  function pushOperatorMP() {
+    sel_op = this.textContent;
+    disp.textContent = sel_op;
+    calc_array.push(sel_op);
+    console.log(calc_array);
 
-      sel_op1 = this.textContent;
-      newnum = num;
-      disp.textContent = newnum + sel_op1;
-      num = "";
-      counter++;
-     }
+    // selected = calc_array.slice((calc_array.indexOf(sel_op))-1), (calc_array.indexOf(sel_op))
+    // disp.textContent = selected.slice(0, 1);
+    // calc_array = selected;
 
-    //function for calculations
-    function calculate() {
-      if (sel_op1 === "+"){
-        total = parseInt(newnum,10) + parseInt(num,10);
 
-      }
-      else if (sel_op1 === "-"){
-        total = parseInt(newnum,10) - parseInt(num,10);
+  }
+  function pushOperatorD() {
+    sel_op = this.textContent;
+    calc_array.push(sel_op);
+    disp.textContent = sel_op;
+    console.log(calc_array);
 
-      }
-      else if (sel_op1 === "/"){
-        total = parseInt(newnum,10) / parseInt(num,10);
 
-      }
-      else if (sel_op1 === "*"){
-        total = parseInt(newnum,10) * parseInt(num,10);
+    // selected = calc_array.slice((calc_array.indexOf(sel_op))-1), (calc_array.indexOf(sel_op))
+    // disp.textContent = selected.slice(0, 1);
+    // calc_array = selected;
 
-      }
-     }//end of calculate functions
+  }
 
-     function equalize() {
-        calculate();
-        disp.textContent = total;
-        newnum = "";
-        num = "";
-
-     }
-    //functions fo clear and clearAll
-    function eraseOne() {
-
-    }
-    function eraseAll() {
-      num = "";
-      newnum = "";
-      disp.textContent = "0";
-      num_counter = "";
-      }
 
 
     //numbers events
@@ -151,11 +208,11 @@
      multiply.addEventListener("click", pushOperatorMP);
      divide.addEventListener("click", pushOperatorD);
 
-     //equals/calculate event
-     eq.addEventListener("click", equalize);
-
-     //clear and clearAll events
-     c.addEventListener("click", eraseOne);
-     cA.addEventListener("click", eraseAll);
+    //  //equals/calculate event
+    //  eq.addEventListener("click", equalize);
+    //
+    //  //clear and clearAll events
+    //  c.addEventListener("click", eraseOne);
+    //  cA.addEventListener("click", eraseAll);
 
 }());
